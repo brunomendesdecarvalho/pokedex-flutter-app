4a5254e8ef6fe8696c9568bd8adc3df4cf879935
Bruno Mendes <brunomendesccb@gmail.com>
2021-12-20 08:54:22 -0300

    adjusted project structure

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-import 'app_widget.dart';
-import 'app_module.dart';
-
+import 'app/app_widget.dart';
+import 'app/app_module.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
-          (value) => runApp(ModularApp(module: AppModule(), child: AppWidget())));
+      (value) => runApp(ModularApp(module: AppModule(), child: AppWidget())));
 }
